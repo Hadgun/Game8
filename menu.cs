30df0c33afb1ed1cdc04db674a96362061760096
Muhamad Hadi Gunawan <hadih3791@gmail.com>
2026-04-12 16:59:53 +0700

    MainMenu2
    Menambahkan MainMenu baru

---
+++ menu.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class menu : MonoBehaviour
+{
+    public GameObject menupanel;
+    public GameObject newgamepanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        menupanel.SetActive(true);
+        newgamepanel.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StartButton(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+    public void NewGameButton()
+    {
+        menupanel.SetActive(false);
+        newgamepanel.SetActive(true);
+    }
+    public void BackButton()
+    {
+        menupanel.SetActive(true);
+        newgamepanel.SetActive(false);
+    }
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
