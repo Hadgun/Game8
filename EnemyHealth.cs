96756e457ede9072bb28b0d28212ddbf5bfd774f
Muhamad Hadi Gunawan <hadih3791@gmail.com>
2026-05-16 19:14:56 +0700

    Menambahkan heal enemy

---
+++ EnemyHealth.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int maxHealth = 3;
+    private int currentHealth;
+    
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+    
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        Debug.Log(gameObject.name + " terkena damage! Sisa health: " + currentHealth);
+        
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+    
+    void Die()
+    {
+        Debug.Log(gameObject.name + " mati!");
+        Destroy(gameObject);
+    }
+}
